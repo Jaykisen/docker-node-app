9899bdf38a80e66090425962d4b74b1d2e691260
Jaykisen <jaykisensahoo92@gmail.com>
2025-05-07 12:29:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Dockerized Node.js App!');
+  res.end('Hello from Dockerized THIS MY SECOND COMMIT FOR PUNE Node.js App!');
 });
 
 server.listen(3000, () => {
